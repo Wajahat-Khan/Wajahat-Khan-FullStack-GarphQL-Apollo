2e53987befeef342671239f895c5800b6e8d1a68
Wajahat Hussain <wajahat.hussain@apple.local>
2020-02-28 13:18:22 +0500

    Basic Sequelize is implemented

--- src/Shared/dbConnection.js
+++ src/Shared/dbConnection.js
@@ -1,5 +1,76 @@
 require('dotenv').config();
 const Client = require('pg').Client;
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize(`${process.env.DATABASE}`, `${process.env.USER_NAME}`, `${process.env.PASSWORD}`, {
+  host: `${process.env.CONNECTION_STRING}`,
+  dialect: 'postgres',
+  define:{
+    timestamps: false
+  }
+
+  });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+  const Employee = sequelize.define('employee', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    salary: {
+      type: Sequelize.INTEGER
+    },
+    gender: {
+      type: Sequelize.STRING
+    },
+    age: {
+      type: Sequelize.INTEGER
+    },
+    experience: {
+      type: Sequelize.INTEGER
+    },
+    role: {
+      type: Sequelize.STRING
+    }
+  }, {
+    freezeTableName: true,
+    paranoid: true
+  });
+
+  Employee.sync().then(()=>{
+    Employee.create({
+      name:'WajahatSEQ',
+      salary:120000,
+      gender:'M',
+      age:22,
+      experience:2,
+      role:"SSE"
+    }).then(newEmp=>{
+      console.log(newEmp)
+      return
+    })
+  }).catch(err=>{
+    console.log("Error ",err);
+  })
+
+
+
+
+
 
 const client = new Client({
   host: process.env.CONNECTION_STRING,
@@ -16,4 +87,4 @@ client.connect(err => {
     console.log('Connection Successful to RDS !!!')
   }
 })
-module.exports = { client };
+module.exports = { client, sequelize };
